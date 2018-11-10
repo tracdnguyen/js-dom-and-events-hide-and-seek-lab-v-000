2cbb8d8fb693b576676d9849ca0e7417f4d96ed8
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-10 01:31:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ function deepestChild() {
   const deepest = document.getElementById('grand-node').querySelector('div div div div div');
   return deepest;
 }
+
+function increaseRankBy(n) {
+  const increase = document.getElementByClassName('ranked-list').querySelectorAll('ul.ranked-list li li li')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + n).toString()
+  }
+}
